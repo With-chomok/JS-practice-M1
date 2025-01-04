e9b8766dcf4cf61f617d0c1648d9bfd5b9b9fc2b
With-chomok <183892208+With-chomok@users.noreply.github.com>
2025-01-04 20:53:47 +0600

    function and array practice

--- chap 1 - all repeat/vari.js
+++ chap 1 - all repeat/vari.js
@@ -1,20 +1,22 @@
-// // let person = "dipol",
-// //       hence = "hablu",
-// //       she = "programmer"
+("use strict");
 
-// //       console.log(she);
+// let person = "dipol",
+//       hence = "hablu",
+//       she = "programmer"
+
+//       console.log(she);
       
 
 // // variable decalre
 
-// // let hablu = true;
-// // console.log(typeof hablu);
+// let hablu = true;
+// console.log(typeof hablu);
 
-// //  let x = "5";
-// //  let y = 5;
-// //  let z = x + y;
+//  let x = "5";
+//  let y = 5;
+//  let z = x + y;
 
-// // console.log( z);
+// console.log( z);
 
 
 // // object declare/
@@ -146,47 +148,238 @@
 // console.log(parseInt(number));
 
 
-// arrey declare
-const cars = ["dipol" , "das"];
+// // arrey declare
+// const cars = ["dipol" , "das"];
 
-let info = cars.at(1);
-// console.log(info);
-// info.join(" #");
-// concat mthod
-const myLife = [ "dipol ", "das", "chomok" , "ghablu", "programmer"];
-const myLife2 = ["this "," my life"];
-// console.log(myLife.concat(myLife2));
-
-
-
-// flat method/
-let myLife3 = ["dipol", "das", "chomok", "hablu", "programmer", "this", "is", "my", "life"];
-let myLife4 = myLife3.flat(2);
-// console.log(myLife4);
-
-// flat method
-
-const myRoll = [[1,2], [3,4],[5,6],[7,8],[9,10]];
-const myRoll1 = myRoll.flat(2);
-console.log(
-    myRoll1
-);
+// let info = cars.at(1);
+// // console.log(info);
+// // info.join(" #");
+// // concat mthod
+// const myLife = [ "dipol ", "das", "chomok" , "ghablu", "programmer"];
+// const myLife2 = ["this "," my life"];
+// // console.log(myLife.concat(myLife2));
 
 
-//length method
 
-const fruits = ["apple ", "banana", "mango" , "orange " ,"apple ", "banana", "mango" , "orange "  ,"banana", "mango" , "orange " ,"apple ", "banana", "mango" , "orange "];
-console.log(fruits.length);
+// // flat method/
+// let myLife3 = ["dipol", "das", "chomok", "hablu", "programmer", "this", "is", "my", "life"];
+// let myLife4 = myLife3.flat(2);
+// // console.log(myLife4);
 
-// join method
-const mfruits = ["Banana", "Orange", "Apple", "Mango"];
-console.log(mfruits.join(" $ "));
+// // flat method
 
-// pop methodd
+// const myRoll = [[1,2], [3,4],[5,6],[7,8],[9,10]];
+// const myRoll1 = myRoll.flat(2);
+// console.log(
+//     myRoll1
+// );
 
-const fruits2 = [ "mango", "apple ", "bana", "orange", "guava"];
-let frut = (fruits2.pop());
-console.log(
-    frut
-);
-console.log(fruits2);
+
+// //length method
+
+// const fruits = ["apple ", "banana", "mango" , "orange " ,"apple ", "banana", "mango" , "orange "  ,"banana", "mango" , "orange " ,"apple ", "banana", "mango" , "orange "];
+// console.log(fruits.length);
+
+// // join method
+// const mfruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(mfruits.join(" $ "));
+
+// // pop methodd
+
+// const fruits2 = [ "mango", "apple ", "bana", "orange", "guava"];
+// let frut = (fruits2.pop());
+// console.log(
+//     frut
+// );
+// console.log(fruits2);
+
+
+// // splice method/
+// const array1 = [ "mango", "apple ", "bana", "orange", "guava"];
+// console.log(array1.splice(0,3));
+
+
+
+
+
+// const splicee = [ "dipol", "chomok", "shaym", "nittynanda", "gourkumar", "sukal"];
+// console.log(splicee.splice(0,5));
+
+// spliced methodd
+// const spli = [ "dipol", "chomok", "shaym", "nittynanda", "gourkumar", "sukal"];
+// console.log(spli.spilced(1,4));
+
+
+
+
+
+// let myDate = new Date(3/4/25)
+// console.log(myDate);
+
+// const coverCeil= ["sdf", "asfd", "asdf", "asdf", "ert", "dfhg", "db"]
+// let ceil = Math.ceil(1,2,3,4,5,6,7)
+// console.log(ceil);
+
+// const d = new Date();
+// console.log(d);
+
+// const da =new Date("2024,01,25, 11:23:43");
+// console.log(da);
+// const d = new Date("2018, 11, 12, 11, 14, 12");
+// console.log(d.getFullYear());
+
+
+// const myDate = new Date();
+
+// let  curretdate = myDate.getFullYear();
+// let  curretday = myDate.getDay();
+// let currentMonth = myDate.getMonth();
+// let currenTime = myDate.getTime();
+// console.log(curretdate);
+// console.log(curretday);
+// console.log(currentMonth);
+// console.log(currenTime);
+
+// let currentMenuit = myDate.getMinutes();
+// console.log(currentMenuit);
+
+// let currenSecond = myDate.getSeconds();
+// console.log(currenSecond);
+// let currentHour = myDate.getHours();
+
+// console.log(currentHour);
+
+// let currenSecondset =  myDate.setSeconds(23);
+// console.log(currenSecondset);
+
+// let currentHourset = myDate.setHours(4);
+// console.log(currenSecondset);
+
+
+// let ceil = Math.ceil(4.5);
+// console.log(ceil);
+// let round = Math.round(7.7);
+
+// console.log(round);
+// let floor = Math.floor(3.9);
+// console.log(floor);
+// let random = Math.random(6);
+// console.log(random);
+// let trunc = Math.trunc(9.1);
+// console.log(trunc);
+
+
+// // first class citizen
+// function sum(a, b) {
+//     return a + b;
+
+// }
+// let add = sum(8, 2);
+// console.log(add);
+
+// function added (x, y) {
+//     return x + y;
+
+// }
+// let adds = sum;
+// console.log(adds(8, 5));
+
+// function sub(a, b) {
+//     return a - b;
+// }
+// let subs = sub;
+// console.log(subs(6, 3));
+
+
+// let mul = function ( a, b ){
+//     return a * b;
+// }
+// let muls = mul ( 6,6 );
+// console.log(muls);
+
+// // nested function
+
+// function sajib(a) {
+//     function dipol (b){
+//         function chomok (c) {
+//             console.log(a + b + c);
+            
+//         }
+//         chomok(" chomok is the ceo of the  company \n" )
+
+        
+//     }
+//     dipol(" dipol is the manager of the company \n")
+    
+// }
+
+// sajib(" sajib is founder of the company \n")
+
+
+// // pass by value and pass by reference
+// function dipol  (a) {
+//     return a = a * a
+// }
+ 
+// let a = 10;
+
+// let chomok = dipol(a)
+// console.log(chomok);
+
+// // practice 2
+
+// function dipol1 (b) {
+//     return b = b + b;
+
+// }
+
+// let b = 10;
+
+// let chomoks = dipol1(b);
+// console.log(chomoks);
+// pass by reference
+
+// Object declare
+
+// const person = {
+//     name : "dipol",
+//     roll : 45643254,
+//     adress : "mymensingh",
+//     age :45
+// }
+
+// function increaseValue(a){
+//     a.age = a.age + 1;
+// }
+// increaseValue(person);
+// console.log(typeof person);
+
+// recursive function 
+
+// function factorial (n) {
+//     if (n == 0) {
+//         return 1;
+//     }
+//     else {
+//         return n * factorial (n - 1);
+
+//     }
+
+
+// }
+// let result = factorial (3);
+// console.log(result);
+
+// recursive FunctionElement
+
+// function dipol2 (n) {
+//     if (n === 0) return 
+//         console.log(n);
+//         dipol2(n - 1);
+
+        
+//     }
+
+// dipol2(10000);
+
+
